e46ae3709e84ba97efc48a61d46ac21c73a1e410
amitmohleji <amit.mohleji@gmail.com>
2015-09-16 14:46:44 -0400

    minor changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 To view the top level packages so you can decide what to remove : jar -tvf com.microsoft.tfs.sdk-11.0.0.jar |cut -c37-200 | cut -d/ -f1,2 | uniq
 
+To fnd all the unique file extensions in a folder hierachy : find Pictures/Photos\ Library.photoslibrary/Masters/ -type f -exec basename {} \; | cut -d"." -f2 |tr '[:upper:]' '[:lower:]' | sort |uniq
+
 To remove classes from jar :  zip com.microsoft.tfs.sdk-11.0.0.jar -d `jar -tvf com.microsoft.tfs.sdk-11.0.0.jar |grep org/apache |cut -c 37-200`
--- restructure_style1.sh
+++ restructure_style1.sh
@@ -4,16 +4,20 @@ for item in `find $1/ -type f`
 do
 yyyymmdd=`mdls -name kMDItemContentCreationDate $item | cut -d" " -f3`
 extension=`basename "$item" | cut -d"." -f2 | tr '[:upper:]' '[:lower:]'` 
+filename=`basename "$item" | cut -d"." -f1` 
 yyyy=`echo $yyyymmdd | cut -d"-" -f1`
 mm=`echo $yyyymmdd | cut -d"-" -f2`
 dd=`echo $yyyymmdd | cut -d"-" -f3`
 
-if [ $extension == 'mov' ] || [ $extension == 'mp4' ] || [ $extension == '3gp' ];
-then
-	folder=$yyyy/$mm/video	
-else
-	folder=$yyyy/$mm/$dd
-fi	
-mkdir -p $folder
-cp -av $item $folder/
+if [ "$filename" != "" ];
+	then
+		if [ $extension == 'mov' ] || [ $extension == 'mp4' ] || [ $extension == '3gp' ];
+		then
+			folder=$yyyy/$mm/video	
+		else
+			folder=$yyyy/$mm/$dd
+		fi	
+		mkdir -p $folder
+		cp -av $item $folder/
+	fi
 done
