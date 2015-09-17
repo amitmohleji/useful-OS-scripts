2adaba4d528944f6c3ee5dae11ac3a059648695f
amitmohleji <amit.mohleji@gmail.com>
2015-09-17 19:08:32 -0400

    new versions

--- restructure_style1_cp.sh
+++ restructure_style1_cp.sh
@@ -3,7 +3,8 @@ IFS=$'\n'
 for item in `find $1/ -type f`
 do
 yyyymmdd=`mdls -name kMDItemContentCreationDate $item | cut -d" " -f3`
-extension=`basename "$item" | cut -d"." -f2 | tr '[:upper:]' '[:lower:]'` 
+#yyyymmdd=`mdls -name kMDItemContentModificationDate $item | cut -d" " -f3`
+extension=`basename "$item" | rev | cut -d"." -f1 | rev | tr '[:upper:]' '[:lower:]'` 
 filename=`basename "$item" | cut -d"." -f1` 
 yyyy=`echo $yyyymmdd | cut -d"-" -f1`
 mm=`echo $yyyymmdd | cut -d"-" -f2`
@@ -11,7 +12,7 @@ dd=`echo $yyyymmdd | cut -d"-" -f3`
 
 if [ "$filename" != "" ];
 	then
-		if [ $extension == 'mov' ] || [ $extension == 'mp4' ] || [ $extension == '3gp' ];
+		if [ $extension == 'mov' ] || [ $extension == 'mp4' ] || [ $extension == '3gp' ] || [ $extension == 'avi' ];
 		then
 			folder=$yyyy/$mm/video	
 		else
